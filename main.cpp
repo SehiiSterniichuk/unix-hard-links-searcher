0c85b779d2393f1ffe8ab9b2a1ef792df10d1a68
SerhiiSterniichuk <sterniichuk.serhii@gmail.com>
2022-05-17 16:33:05 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -3,21 +3,51 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include "math.h"
+#include "vector"
 
-#define CONSOLE_CAPACITY 80//максимальне число символів для найбільшого результату гістограми
+#define MIN_LINKS_TO_FILE 2//мінімальне число жорстких посилань
+
 using namespace std;
 
-const int interval = 1024 / 2;//інтервал
-map<string, int> statistic;//string - інтервал, int - лічильник файлів, що входять у цей інтервал
-long maxCounter = 0;//максимальна кількість файлів, що потрапляла у проміжок
+struct Node {//структура, що зв'язує серійний номер файлу (st_ino) та шляхи до файлів
+    int linkID;//серійний номер файлу
+    vector<string> *paths;//шляхи до файлів
+    int numberOfLinks;// кількість посилань
 
-void countSizeToRange(int size);/*знаходимо проміжок якому належить розмір файлу
- * та інкрементуємо лічильник проміжку*/
+    Node(const int &linkID, const int &numberOfLinks, const string &path) {
+        this->linkID = linkID;
+        paths = new vector<string>();
+        this->numberOfLinks = numberOfLinks;
+        push(path);
+    }
 
-void countFile(struct stat &file) {//дізнаємося розмір файлу
-    int size = file.st_size;
-    countSizeToRange(size);
+    void push(const string &path) {
+        paths->push_back(path);
+    }
+
+    void print() {
+        for (string &path: *paths) {
+            cout << "Link:\t| " << linkID << " |\tNumber of links:\t| " << numberOfLinks
+                 << " |\tpath:\t\" " << path << "\"" << endl;
+        }
+    }
+
+    ~Node() {
+        delete paths;
+    }
+};
+
+map<int, Node *> nodes;//int - link
+
+void scanFile(const struct stat &buf, string &path) {
+    int key = buf.st_ino;//дістаємо серійний номер, що і буде ключем
+    if (nodes.contains(key)) {
+        auto node = nodes.find(key);
+        node->second->push(path);//якщо даний серійний номер вже зустрічався, то просто додаємо шлях у наявну Node
+        return;
+    }
+    //інакше створюємо нову Node
+    nodes.insert(pair(key, new Node(key, buf.st_nlink, path)));
 }
 
 void scanAllFilesInDirectory(const string &path) {
@@ -43,15 +73,16 @@ void scanAllFilesInDirectory(const string &path) {
  * і записує її в область, на яку вказує аргумент buf
  * Після успішного завершення lstat() повертає 0.
  * В іншому випадку він повертає -1 і встановлює errno, щоб вказати помилку.*/
+            string filePathString(filePath);
             if (S_ISDIR(buf.st_mode)) { //S_ISDIR - Цей макрос повертає int відмінний від нуля, якщо файл є каталогом.
                 if (strcmp(entry.d_name, ".") &&
                     strcmp(entry.d_name, "..")) { // Пропускаємо папки "." та ".."
-                    string nextFolder(filePath);
-                    scanAllFilesInDirectory(nextFolder); //рекурсивно заходимо в неї
+                    scanAllFilesInDirectory(filePathString); //рекурсивно заходимо в неї
                 }
-            } else if (S_ISREG(buf.st_mode)) {
+            } else if (S_ISREG(buf.st_mode) != 0 && buf.st_nlink >= MIN_LINKS_TO_FILE) {
                 //S_ISREG - Цей макрос повертає відмінний від нуля, якщо файл є звичайним.
-                countFile(buf);
+                // buf.st_nlink - кількість жорстких посилань на файл
+                scanFile(buf, filePathString);
             }
         } else {//lstat не зміг отримати інформацію про файл і повернув не 0
             cout << "Error in lstat() through" << filePath << endl;
@@ -61,62 +92,17 @@ void scanAllFilesInDirectory(const string &path) {
     closedir(dirp);//закриваємо потік
 }
 
-void printResult();
+void printAll() {
+    for (auto &it: nodes) {
+        it.second->print();
+        delete it.second;
+        cout << endl;
+    }
+}
 
 int main() {
-    string path = "/home/serhii/Documents";
+    string path = "/home/serhii";
     scanAllFilesInDirectory(path);
-    printResult();
+    printAll();
     return 0;
-}
-
-string rangeToString(int start, int finish) {
-    return to_string(start) + "-" + to_string(finish);
-}
-
-string getRange(int value) {//знаходимо проміжок якому належить даний розмір
-    if (value <= interval) {
-        return rangeToString(1, interval);
-    }
-    int start, finish;
-    if (value % interval == 0) {
-        start = value - interval + 1;
-        return rangeToString(start, value);
-    }
-    int diff = value - value % interval;
-    start = diff + 1;
-    finish = diff + interval;
-    return rangeToString(start, finish);
-}
-
-void countSizeToRange(int size) {//функція для підрахунку кількості файлів, що належать кожному проміжку
-    string key = getRange(size); //обраховуємо проміжок якому він належить - це і є ключ.
-    // А далі додаємо у мапу або інкрементуємо значення лічильника, якщо у цьому проміжку вже хтось був
-    int counter;
-    if (!statistic.contains(key)) {
-        counter = 1;
-        statistic.insert(pair(key, counter));
-    } else {
-        auto node = statistic.find(key);
-        counter = node->second + 1;
-        node->second = counter;
-    }
-    if (counter > maxCounter) { maxCounter = counter; }
-}
-
-void printStarLine(int stars) {
-    for (int i = 0; i < stars; ++i) { cout << "*"; }
-    cout << endl;
-}
-
-void printResult() {
-    if (maxCounter == 0) { return; }
-    double coeff = (double) CONSOLE_CAPACITY / ((double) maxCounter);
-    for (auto &node: statistic) {
-        int size = node.second;
-        if (size != 0) {
-            cout << node.first << endl;
-            printStarLine(max((size * coeff), 1.0));
-        }
-    }
 }
